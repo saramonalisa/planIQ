37d0a8ab9e4342bad620f773ce3f447d9a7a25cc
gaabyssouza <162709401+gaabyssouza@users.noreply.github.com>
2025-09-16 10:00:21 -0300

    model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=200) 
+    descricao = models.TextField(blank=True, null=True) 
+    concluida = models.BooleanField(default=False)  
+    data_criacao = models.DateTimeField(auto_now_add=True) 
+    prazo = models.DateField(blank=True, null=True) 
+    def __str__(self):
+        return self.titulo
